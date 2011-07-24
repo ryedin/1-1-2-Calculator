9794bc243df0f719ed2e0d5182e7b0ca15b22de2
ryedin <ryan.gahl@gmail.com>
2011-07-24 10:57:14 -0500

    fixed devdeply script to properly update package version

--- devdeploy.sh
+++ devdeploy.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 palm-install -r ryan.gahl.1plus1calculator.tp
 ./package.sh
-palm-install packages/ryan.gahl.1plus1calculator.tp_1.0.0_all.ipk
+VERSION=`cat appinfo.json | egrep -o '"version":\s"([^"]*)"' | sed -r 's/"version":\s"([^"]*)"/\1/'`
+PACKAGE_NAME="ryan.gahl.1plus1calculator.tp_"$VERSION"_all.ipk"
+palm-install packages/$PACKAGE_NAME
 palm-launch ryan.gahl.1plus1calculator.tp
